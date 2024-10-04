1372ffbf0df3724b04a1ba9d4ee98a62cb8e97a8
Anamaria Miranda <anny13miranda@gmail.com>
2024-10-04 15:59:15 +0200

    Added TP1 solutions

--- TP1/cousine.cpp
+++ TP1/cousine.cpp
@@ -3,8 +3,18 @@
 
 int main(int agrc, char **argv) {
 	double x1 = 5;
-	float x2 = 3.1;
-	double cosd = cos(double x1);
-	float cosf = cos(float x2);
-	//long double cosl(long double x3);
+	float x2 = 3.1f;
+	long double x3 = 3L;
+	double cosd = cos(x1);
+	float cosf = cos(x2);
+	long double cosl = cos(x3);
+
+	double x4;
+	std::cin >> x4;
+
+	std::cout << cosd << std::endl;
+	std::cout << cosf << std::endl;
+	std::cout << cosl << std::endl;
+	std::cout << "Coseno" << cos(x4) << std::endl;
+
 }
